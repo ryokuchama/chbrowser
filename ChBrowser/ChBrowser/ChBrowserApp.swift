4c81a6f30a34b8061e99135847368c02ea82c906
ryokuchama <r.madrid.pg@gmail.com>
2023-07-11 21:45:50 +0900

    chore: first commit

---
+++ ChBrowser/ChBrowser/ChBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChBrowserApp.swift
+//  ChBrowser
+//
+//  Created by 梶健将 on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct ChBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
